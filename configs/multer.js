18d439a5abb49791d9a40b7fe8b46e084300b4ef
AeroByteInnovationsPvtLtd <aerobyteinnovations@gmail.com>
2026-03-05 13:46:00 +0530

    Remove accidental deployment from main branch

--- configs/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-// Use memoryStorage for cloud uploads (like ImageKit)
-const storage = multer.memoryStorage();
-
-// Pass the correctly spelled 'storage' variable here
-const upload = multer({ storage });
-
-export default upload;
